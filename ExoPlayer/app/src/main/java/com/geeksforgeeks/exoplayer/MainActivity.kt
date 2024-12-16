1dda2515f0be6340b43d60cccf7706aaac9c8bad
Uriel Frankel <uriel@riverside.fm>
2024-12-16 10:04:06 +0200

    testing media source

--- ExoPlayer/app/src/main/java/com/geeksforgeeks/exoplayer/MainActivity.kt
+++ ExoPlayer/app/src/main/java/com/geeksforgeeks/exoplayer/MainActivity.kt
@@ -1,47 +1,63 @@
-package com.geeksforgeeks.exoplayer;
+package com.geeksforgeeks.exoplayer
 
-import android.net.Uri;
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.media3.common.MediaItem;
-import androidx.media3.exoplayer.ExoPlayer;
-import androidx.media3.ui.PlayerView;
-
-
-public class MainActivity extends AppCompatActivity {
-
-    private ExoPlayer player;
-    private PlayerView playerView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+import android.os.Bundle
+import androidx.annotation.OptIn
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.viewmodel.CreationExtras.Empty.map
+import androidx.media3.common.MediaItem
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.FileDataSource
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.MediaSource
+import androidx.media3.exoplayer.source.MergingMediaSource
+import androidx.media3.exoplayer.source.ProgressiveMediaSource
+import androidx.media3.ui.PlayerView
 
 
-        playerView = findViewById(R.id.player_view);
+class MainActivity : AppCompatActivity() {
+    private var player: ExoPlayer? = null
+    private var playerView: PlayerView? = null
+
+    @OptIn(UnstableApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        playerView = findViewById(R.id.player_view)
 
         // Initialize ExoPlayer
-        player = new ExoPlayer.Builder(this).build();
-        playerView.setPlayer(player);
+        player = ExoPlayer.Builder(this).build()
+        playerView?.setPlayer(player)
 
+        val list = arrayListOf(
+            "asset:///a01.mp4",
+            "asset:///v01.mp4",
+            "asset:///a02.mp4",
+            "asset:///v02.mp4",
+            "asset:///a03.mp4",
+            "asset:///v03.mp4",
+            "asset:///a04.mp4",
+            "asset:///v04.mp4",
+        )
         // Build the MediaItem
-        String videoUrl = "https://media.geeksforgeeks.org/wp-content/uploads/20201217163353/Screenrecorder-2020-12-17-16-32-03-350.mp4";
-        Uri uri = Uri.parse(videoUrl);
-        MediaItem mediaItem = MediaItem.fromUri(uri);
+
 
         // Prepare the player with the media item
-        player.setMediaItem(mediaItem);
-        player.prepare();
-        player.setPlayWhenReady(true); // Start playing when ready
+        val items = list.map {ProgressiveMediaSource.Factory(FileDataSource.Factory()).createMediaSource(MediaItem.fromUri(it))}
+
+        val i = items.toTypedArray()
+        val mediaSource = MergingMediaSource(false, *i)
+        player!!.setMediaSource(mediaSource)
+        player!!.prepare()
+        player!!.playWhenReady = true // Start playing when ready
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
+    override fun onStop() {
+        super.onStop()
         if (player != null) {
-            player.release();
-            player = null;
+            player!!.release()
+            player = null
         }
     }
 }
